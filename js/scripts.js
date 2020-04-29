13413922ad328f63d8990143fd77b2ebbeb8cc00
MarielHamson <mariel.hamson@gmail.com>
2020-04-28 20:34:46 -0700

    change format to highlighting

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
   var height = parseInt(prompt("Please enter your height in inches."));
 
   if (height >= 60) {
-    $("#Everyone").show();
-    $("#Adults-Only").show();
+    $("#Everyone").addClass("allowed");
+    $("#Adults-Only").addClass("allowed");
   } else if (height >=48 && height <60) {
-    $("#Everyone").show();
-    $("#Kids-Only").show();
-    $("#Babies-Only").show();
+    $("#Everyone").addClass("allowed");
+    $("#Kids-Only").addClass("allowed");
+    $("#Babies-Only").addClass("allowed");
   } else if (height >=42 && height <48) {
-    $("#Kids-Only").show();
-    $("#Babies-Only").show();
+    $("#Kids-Only").addClass("allowed");
+    $("#Babies-Only").addClass("allowed");
   } else {
-      $("#Babies-Only").show()
+      $("#Babies-Only").addClass("allowed")
     }
 });
